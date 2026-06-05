8adb46cd22449b9533633dbefc0eb1e240c0197e
anton-mp <abramova2712katya@gmail.com>
2026-06-05 09:52:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
-#include  "tree.h"
+#include "tree.h"
 
 int fact(int n) {
     int res = 1;
@@ -20,10 +20,14 @@ std::vector<std::vector<char>> getAllPerms(PMTree& tree) {
 }
 
 std::vector<char> getPerm1(PMTree& tree, int num) {
-    std::vector<std::vector<char>> perms = getAllPerms(tree);
-    if (num <= 0 || num > perms.size())
+    PMTree::Node* root = tree.getRootNode();
+    int size = root->children.size();
+    if (num <= 0 || num > fact(size))
         return {};
-    return perms[num-1];
+    std::vector<char> buffer;
+    std::vector<std::vector<char>> perms;
+    tree.createBufferPerms(root, buffer, perms, num);
+    return perms[num - 1];
 }
 
 std::vector<char> getPerm2(PMTree& tree, int num) {
